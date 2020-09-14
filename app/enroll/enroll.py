796c5728fecf41301d76b1df398ba3b427ae8d28
mutembei-lewis <teshlewie-668@gmail.com>
2020-09-14 05:30:00 -0400

    removing blueprints

--- app/enroll/enroll.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint 
-enroll = Blueprint("enroll", __name__, url_prefix = "/enrollment")
-# the blueprint function takes three argument = name of the print, 
-@enroll.route("/register")
-def register():
-    return "<h3> This is the registration page <h3>"
